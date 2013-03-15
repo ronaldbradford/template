1eb3039921762b7a5a0835c2d8d0e510a73ae4f5
Ronald Bradford <ronald.bradford@techcofounders.com>
2013-03-15 12:27:24 -0400

    Initial Code Base

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-template
+Template
 ========
 
-Base template for HTML5/Bootstrap/JQuery web app
+Base template for HTML5/Bootstrap/JQuery web app using PHP includes for removing code duplication.
+
+All modules are included so this can run locally without network access.
---
+++ www/m/j/default.js
@@ -0,0 +1,29 @@
+String.prototype.reverse = function () {
+    return this.split("").reverse().join("");
+}
+
+function setCookie(c_name,value,exdays) {
+  var exdate=new Date();
+  exdate.setDate(exdate.getDate() + exdays);
+  var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString()) + "; path=/";
+  document.cookie=c_name + "=" + c_value;
+}
+
+$.urlParam = function(name){
+    var results = new RegExp('[\\?&amp;]' + name + '=([^&amp;#]*)').exec(window.location.href);
+    return decodeURI(results[1]) || 0;
+}
+
+$.fn.clearForm = function() {
+  return this.each(function() {
+    var type = this.type, tag = this.tagName.toLowerCase();
+    if (tag == 'form')
+      return $(':input',this).clearForm();
+    if (type == 'text' || type == 'password' || tag == 'textarea')
+      this.value = '';
+    else if (type == 'checkbox' || type == 'radio')
+      this.checked = false;
+    else if (tag == 'select')
+      this.selectedIndex = -1;
+  });
+};
